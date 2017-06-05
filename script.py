ba29d9e228ff6c53b751a7378e1de8bb1d4b1271
Venkatesh K <venkatesh.k@appviewx.com>
2017-06-05 17:26:23 +0530

    script has changed

--- script.py
+++ script.py
@@ -3,6 +3,6 @@ import commands
 result = commands.getstatusoutput("python test.py")
 
 if result[0] == 0 and "FAIL:"not in result[1]:
-    print "Test Pass."
+    print "Test Pass:"
 else:
     print "Test Fails."
